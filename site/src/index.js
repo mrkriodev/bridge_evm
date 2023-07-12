0ce3c1fa6129dced0866e5d05b48bfcf76ec0a15
vfaust403 <vl.averkov@mail.ru>
2023-07-12 06:07:11 -0400

    forgor api ip:port

--- site/src/index.js
+++ site/src/index.js
@@ -6,7 +6,6 @@ import App from "./app";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <App URL = "http://92.255.109.253:8081/api/status" /> */}
-        <App URL = "http://127.0.0.1:8000/api/status" />
+        <App URL = "http://92.255.109.253:8081/api/status" />
     </React.StrictMode>
 );
