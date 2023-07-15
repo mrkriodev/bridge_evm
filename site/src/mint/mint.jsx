2b35b90ddbb27396c1ac89a4b47489d4194d106a
vfaust <vl.averkov@mail.ru>
2023-07-15 12:56:17 -0400

    Some checks

--- site/src/mint/mint.jsx
+++ site/src/mint/mint.jsx
@@ -13,9 +13,53 @@ export default class Mint extends Component {
         };
     }
 
+    async SetErrorMessage(Message) {
+        this.setState ({
+            ...this.state, Message: Message
+        });
+
+        setTimeout(this.setState.bind(this), 5000, {
+            ...this.state, Message: null
+        });
+    }
+
     async ClickHandler() {
         let {FirstAddress, Message, To} = this.state;
-        console.log(`this.state.FirstAddress ${FirstAddress} ${Message} ${To}`)
+        console.log(`Preparing transaction with args: ${FirstAddress} ${Message} ${To}`);
+
+        if(FirstAddress === null || FirstAddress === undefined) {
+            let ErrorMessage = `From is ${FirstAddress}`;
+            await this.SetErrorMessage(ErrorMessage);
+            return;
+        }
+
+        if(To === null || To === undefined) {
+            let ErrorMessage = `To is ${To}`;
+            await this.SetErrorMessage(ErrorMessage);
+            return;
+        }
+
+        if(To.length !== 42) {
+            let ErrorMessage = `To has invalid len ${To.lengt}`;
+            await this.SetErrorMessage(ErrorMessage);
+            return;
+        }
+
+        To = Number(To, 10);
+        if(isNaN(To)) {
+            let ErrorMessage = `To is not a number`;
+            await this.SetErrorMessage(ErrorMessage);
+            return;
+        }
+
+        if(FirstAddress.length !== 42) {
+            let ErrorMessage = `From has invalid len ${FirstAddress.lengt}`;
+            await this.SetErrorMessage(ErrorMessage);
+            return;
+        }
+
+        
+        return;
     }
 
     render() {
@@ -32,7 +76,7 @@ export default class Mint extends Component {
                         this.setState ({
                             ...this.state, Message: Event.target.value
                         });
-                    }}/>
+                    }} />
                 </div>
                 <button onClick = {this.ClickHandler.bind(this)} className = "Button">Buy me a Coffee<SiBuymeacoffee className = "Coffee" /></button>
             </div>
